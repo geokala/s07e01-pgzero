45d877f3805c6b02005ffa88541813974ef68b92
Madda <nobody.youknow@local>
2015-09-03 21:24:33 +0100

    Magic movement go!

--- spyceinvaders.py
+++ spyceinvaders.py
@@ -12,6 +12,7 @@ for i in range(5):
     enemy.top = 0
     enemy.centerx = WIDTH/2 + 100*(i - 2)
     enemies.append(enemy)
+    enemy.direction = "right"
 
 def draw():
     screen.clear()
@@ -30,6 +31,19 @@ def update():
             projectiles.remove(projectile)
         elif projectile.bottom <= 0:
             projectiles.remove(projectile)
+    for enemy in enemies:
+        if enemy.direction == "right":
+            enemy.x += 5
+            if enemy.right > WIDTH:
+                enemy.x -= 5
+                enemy.y += 50
+                enemy.direction = "left"
+        elif enemy.direction == "left":
+            enemy.x -= 5
+            if enemy.left < 0:
+                enemy.x += 5
+                enemy.y += 50
+                enemy.direction = "right"
             
 
 def on_key_down(key):
