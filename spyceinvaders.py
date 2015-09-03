32e7812689aeb3609b97f38b78dfe4ceac011a3a
Madda <nobody.youknow@local>
2015-09-03 20:28:52 +0100

    Cake delivery service initialised

--- spyceinvaders.py
+++ spyceinvaders.py
@@ -8,3 +8,10 @@ spyceship.x = WIDTH/2
 def draw():
     screen.clear()
     spyceship.draw()
+
+def on_key_down(key):
+    if key ==keys.LEFT:
+        spyceship.x -= 50
+    if key==keys.RIGHT:
+        spyceship.x+=50
+
