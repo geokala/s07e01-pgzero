c8b68b9304e39b24191dfa9d88152bc7c7328289
Madda <nobody.youknow@local>
2015-09-03 20:47:29 +0100

    Add projectiles to the game

--- spyceinvaders.py
+++ spyceinvaders.py
@@ -4,14 +4,31 @@ HEIGHT = 600
 spyceship = Actor('cake')
 spyceship.bottom = HEIGHT
 spyceship.centerx = WIDTH/2
+projectiles= list()
 
 def draw():
     screen.clear()
     spyceship.draw()
+    for projectile in projectiles:
+        projectile.draw()
+
+def update():
+    for projectile in projectiles:
+        projectile.y -= 10
+        if projectile.bottom <= 0:
+            projectiles.remove(projectile)
 
 def on_key_down(key):
     if key ==keys.LEFT:
         spyceship.x -= 50
     if key==keys.RIGHT:
         spyceship.x+=50
+    if key==keys.SPACE:
+         fire()
+
+def fire():
+    projectile=Actor('long-arrow-up')
+    projectile.midbottom = spyceship.midtop
+    projectiles.append(projectile)
+    
 
